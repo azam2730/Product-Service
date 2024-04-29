a562f82142f9337cf2c35780291321f32a258c5d
azam2730 <azamnishat2001@gmail.com>
2024-04-30 00:06:36 +0530

    2nd commit

--- src/main/java/com/learning/controller/EmployeeController.java
+++ src/main/java/com/learning/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.learning.controller;
 
 import com.learning.entity.EmployeeEntity;
+import com.learning.entity.ProductEntity;
 import com.learning.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -40,4 +41,17 @@ public class EmployeeController {
         return employeeEntities;
 
     }
+
+//    @PutMapping("updateProduct/{id}")
+//    public ProductEntity updateProduct(@PathVariable long id, @RequestBody ProductEntity updatedEntity){
+//        Optional<ProductEntity> savedProduct = productRepository.findById(id);
+//        ProductEntity productEntity = savedProduct.get();
+//        productEntity.setProductName(updatedEntity.getProductName());
+//        productEntity.setProductPrice(updatedEntity.getProductPrice());
+//        productEntity.setAvailable(updatedEntity.isAvailable());
+//        ProductEntity updatedProduct = productRepository.save(productEntity);
+//        return updatedProduct;
+//    }
+
+
 }
